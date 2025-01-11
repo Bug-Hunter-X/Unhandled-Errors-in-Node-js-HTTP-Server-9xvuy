69fbc5dce30aa6b599c585e39947b376a7e05eea
BugHunter-X <askzenai@gmail.com>
2025-01-11 22:44:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js!' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running at http://localhost:8080');
+});
